f2a39a7dfe4b82df186974b8feb34803017cf619
amaliafanda12 <amaliafanda97@gmail.com>
2023-06-22 09:58:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+
+<body>
+    <!-- Site wrapper -->
+    <div class="wrapper">
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="container">
+            <div class="card">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-3">
+                            <div class="ms-4">
+                                <h1>Sistem Kepuasan Siswa Terhadap Tentor di CV. Ibsi Edutama</h1>
+                            </div>
+                            <style>
+                                body {
+                                    font-family: 'Font Name', sans-serif;
+                                }
+                            </style>
+
+                        </div>
+                        <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link" id="custom-tabs-one-home-tab" data-toggle="pill" href="#custom-tabs-one-home" role="tab" aria-controls="custom-tabs-one-home" aria-selected="false">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill" href="#custom-tabs-one-profile" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Profile</a>
+                            </li>
+                        </ul>
+                    </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-body">
+
+                            <body class="hold-transition">
+                                <!-- Site wrapper -->
+                                <div class="container">
+                                    <!-- Navbar -->
+
+                                    <!-- /.navbar -->
+                                    <div class="form-group">
+                                        <label>1. Seberapa baik kualitas pengajaran tentor IBSI? </label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>2. Seberapa baik kedisiplinan tentor IBSI? </label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>3. Menurut Anda, secara keseluruhan, seberapa baik sikap tentor IBSI? </label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>4. Seberapa baik penampilan tentor IBSI? </label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>4. Seberapa baik penampilan tentor IBSI? </label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>5. Seberapa baik penguasaan materi tentor IBSI? </label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>6. Seberapa baik penjelasan tentor IBSI saat mengajar?</label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>7. Seberapa baik suasana belajar yang dibangun tentor saat mengajar?</label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>8. Seberapa baik peningkatan hasil belajar murid setelah mengikuti bimbel di IBSI?</label>
+                                        <select class="form-control">
+                                            <option>Tidak Baik</option>
+                                            <option>Kurang Baik</option>
+                                            <option>Baik</option>
+                                            <option>Sangat Baik</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <!-- /.card -->
+
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+
+        </div>
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+
+            </div>
+            <strong>Copyright Web Kepuasan Siswa
+        </footer>
+
+        <!-- ./wrapper -->
+
+        <!-- jQuery -->
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="dist/js/adminlte.min.js"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="dist/js/demo.js"></script>
+</body>
+
+</html>
