089a85e6c76dbcffe9d8da2ab9e246edaae9bbef
seriusman0 <seriusman0@gmail.com>
2023-06-30 14:19:00 +0700

    update database

--- backup/klasifikasi1.sql
+++ backup/klasifikasi1.sql
@@ -35,7 +35,7 @@ CREATE TABLE `dataset` (
   `a8` int DEFAULT NULL,
   `timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
